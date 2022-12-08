77dedb4237a32ada800a9284692f2ab5ad24f56a
Auto78 <94161885+Auto78@users.noreply.github.com>
2022-12-08 13:21:07 -0800

    Update BlackJack.cpp

--- BlackJack.cpp
+++ BlackJack.cpp
@@ -1,2 +1,16 @@
-//This is the beginning of a game of black jack
+// This is the beginning of a game of black jack
 
+// Preprocessor Directives
+#include <iostream>
+#include <limits>
+#include <vector>
+
+using namespace std;
+// Declared constants
+const int DECK_OF_CARDS = 52;
+const int SUITS = 4;
+
+// Intro to main code
+int main()
+{
+}
